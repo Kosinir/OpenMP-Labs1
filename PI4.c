8bdf786e748e44c60c99811cf3c807c1e9a3fc74
Ezio S. <147484519+Kosinir@users.noreply.github.com>
2024-11-07 18:23:13 +0100

    Update PI4.c

--- PI4.c
+++ PI4.c
@@ -7,38 +7,59 @@ double step;
 
 int main(int argc, char* argv[])
 {
-	clock_t start, stop;
-	double x, pi, sum=0.0;
-	int i;
+    clock_t spstart, spstop, ppstart, ppstop;
+    double sswtime, sewtime, pswtime, pewtime;
+    double pi, sum = 0.0;
+    int i;
 
-//osobny start
-	double start_t=0.0, end_t;
-	start_t= omp_get_wtime();
+    // SEKWENCYJNIE
+    sswtime = omp_get_wtime();
+    spstart = clock();
 
-	step = 1./(double)num_steps;
-	start = clock();
+    step = 1.0 / (double)num_steps;
 
-#pragma omp parallel 
-{ 
-double suml=0.0; //suma lokalna
-#pragma omp for
+    for (i = 0; i < num_steps; i++)
+    {
+        double x = (i + 0.5) * step;
+        sum += 4.0 / (1.0 + x * x);
+    }
+    pi = sum * step;
 
-	for (i=0; i<num_steps; i++)
-	{
-		double x = (i + .5)*step;	
-		suml += 4.0/(1.+ x*x);
-	}
-#pragma omp atomic //poprawne scalenie zmiennej globalnej
-sum+=suml;
-}	
-	pi = sum*step;
-	stop = clock();
+    spstop = clock();
+    sewtime = omp_get_wtime();
+    printf("%15.12f wartosc liczby PI sekwencyjnie \n", pi);
 
-	end_t = omp_get_wtime();
-	
+    // RÓWNOLEGLE
+    pswtime = omp_get_wtime();
+    ppstart = clock();
 
-	printf("Wartosc liczby PI wynosi %15.12f\n",pi);
-	printf("Czas przetwarzania procesorów wynosi %f sekund, wallclock %f sek \n",((double)(stop - start)/CLOCKS_PER_SEC), end_t-start_t);
-	
-	return 0;
+    sum = 0.0; // resetujemy sum, inaczej dostaniemy 2*pi
+
+#pragma omp parallel
+    {
+        double local_sum = 0.0; // prywatna suma dla każdego wątku
+        #pragma omp for
+        for (i = 0; i < num_steps; i++)
+        {
+            double x = (i + 0.5) * step;
+            local_sum += 4.0 / (1.0 + x * x);
+        }
+
+        #pragma omp atomic
+        sum += local_sum; // scalanie wyników pracy każdego wątku
+    }
+
+    pi = sum * step;
+
+    ppstop = clock();
+    pewtime = omp_get_wtime();
+
+    printf("%15.12f wartosc liczby PI rownolegle \n", pi);
+    printf("Czas procesorów przetwarzania sekwencyjnego  %f sekund \n", ((double)(spstop - spstart) / CLOCKS_PER_SEC));
+    printf("Czas procesorów przetwarzania równoleglego  %f sekund \n", ((double)(ppstop - ppstart) / CLOCKS_PER_SEC));
+    printf("Czas trwania obliczen sekwencyjnych - wallclock %f sekund \n", sewtime - sswtime);
+    printf("Czas trwania obliczen rownoleglych - wallclock %f sekund \n", pewtime - pswtime);
+    printf("Przyspieszenie %5.3f \n", (sewtime - sswtime) / (pewtime - pswtime));
+
+    return 0;
 }
